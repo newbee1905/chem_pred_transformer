d4b42193a6dee5c111353160a78188fec06e19f0
newbee1905 <beenewminh@outlook.com>
2025-07-29 08:40:26 +1000

    wip: add new dataset training file

--- finetune_bart_uspto_sep.sh
+++ finetune_bart_uspto_sep.sh
@@ -4,24 +4,30 @@ source .venv/bin/activate
 
 MODEL="bart_small"
 TASK="finetune"
-PRETRAIN_STATE_DICT="pretrain-zinc-${MODEL}_e5_v0.pth"
-NAME="uspto-sep-${MODEL}_v9"
+# PRETRAIN_STATE_DICT="best-checkpoint-pretrain-zinc-${MODEL}_e5_v0.pth"
+# PRETRAIN_STATE_DICT="best-exact-ppo.pth"
+# PRETRAIN_STATE_DICT="bart_uspto_finetuned_v8-v8.pth"
+PRETRAIN_STATE_DICT="bart_uspto_finetuned_v8-tanimoto-v8.pth"
+NAME="private-${MODEL}"
 
+# dataset=uspto +dataset.mode=sep task=train task_type=finetune \
 # +pretrained_state_dict="best-checkpoint-$PRETRAIN_STATE_DICT" \
 # +ckpt_path="train_checkpoints/best-checkpoint-finetune-uspto-sep-${MODEL}_small_v0.ckpt" \
+# +ckpt_path="train_checkpoints/best-checkpoint-finetune-uspto-sep-${MODEL}_aux_3-v1.ckpt" \
+# pth_path="best-checkpoint-$TASK-$NAME.pth" \
+# pth_path="best-exact-ppo.pth" \
+# pth_path="bart_uspto_finetuned_v8-v8.pth" \
 
 #	+model.aux_head=true +dataset.collator.aux_head=true \
 #	+dataset.collator.aux_prop_stats_path=aux_prop_stats.pickle \
 
 HYDRA_FULL_ERROR=1 python main.py model="$MODEL" \
-	+pretrained_state_dict="best-checkpoint-$PRETRAIN_STATE_DICT" \
-	dataset=uspto +dataset.mode=sep task=train task_type=finetune \
-	+ckpt_path="train_checkpoints/best-checkpoint-finetune-uspto-sep-${MODEL}_aux_3-v1.ckpt" \
-	pth_path="best-checkpoint-$TASK-$NAME.pth" \
+	+pretrained_state_dict="$PRETRAIN_STATE_DICT" \
+	dataset=private task=train task_type=finetune \
 	callbacks.1.filename="best-checkpoint-$TASK-$NAME" \
 	callbacks.2.filename="best-checkpoint-$TASK-$NAME-tanimoto" \
 	callbacks.3.filename="last-epoch-checkpoint-$TASK-$NAME" \
 	loggers.0.name="$TASK-$NAME" \
 	+trainer.max_epochs=100 \
-	num_workers=16
+	num_workers=16 \
 	+kv_cache=true
--- submit_test.sh
+++ submit_test.sh
@@ -6,18 +6,28 @@ source .venv/bin/activate
 # CKPT_PATH="last-epoch-checkpoint-finetune-uspto-sep-bart_small_v5.ckpt"
 # CKPT_PATH="best-checkpoint-finetune-uspto-sep-bart_small_v7-tanimoto-v3.ckpt"
 # CKPT_PATH="best-checkpoint-finetune-uspto-sep-chemformer_small_v8-tanimoto-v1.ckpt"
-CKPT_PATH="best-checkpoint-finetune-uspto-sep-bart_small_v8-v8.ckpt"
+# CKPT_PATH="best-checkpoint-finetune-uspto-sep-bart_small_v8-v8.ckpt"
+# CKPT_PATH="best-checkpoint-finetune-private-rotate-bart_small.ckpt"
+# CKPT_PATH="best-checkpoint-finetune-private-bart_small-v8.ckpt"
+CKPT_PATH="best-checkpoint-finetune-private-bart_small-tanimoto-v31.ckpt"
+# CKPT_PATH="last-epoch-checkpoint-finetune-private-bart_small-v9.ckpt"
 MODEL="bart_small"
+PRETRAIN_STATE_DICT="bart_uspto_finetuned_v8-v8.pth"
 
 	# +ckpt_path=train_checkpoints/${CKPT_PATH} \
+	# +ckpt_path=train_checkpoints_server/${CKPT_PATH} \
+
+	# dataset=uspto \
+	# +dataset.mode=sep \
+
+	# dataset=private_rotate \
 
 python main.py \
+	+ckpt_path=train_checkpoints/${CKPT_PATH} \
 	num_workers=16 \
 	task=test \
-	+ckpt_path=train_checkpoints_server/${CKPT_PATH} \
-	dataset=uspto \
-	+dataset.mode=sep \
 	model=$MODEL \
+	dataset=private \
 	+module.sampler=beam \
-	+module.beam_size=20 \
+	+module.beam_size=10 \
 	batch_size=4
